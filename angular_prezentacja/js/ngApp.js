5cec847058feb1044a420dfbae013437f1b57ddd
pcierpiatka <p.cierpiatka@gmail.com>
2014-06-08 16:55:20 +0200

    ¢scope + data binding

--- angular_prezentacja/js/ngApp.js
+++ angular_prezentacja/js/ngApp.js
@@ -1,10 +1,18 @@
 angular.module('ngApp', [])
     .controller('ScopeController',['$scope',function($scope){
-
         $scope.elements = ['pierwszy','drugi','trzeci'];
 
         $scope.add = function(){
             $scope.elements.push($scope.element);
             $scope.element = "";
+
         };
+    }])
+
+    .controller('ScopeFirstController',['$scope', function($scope){
+        $scope.data = {message:"juz jestem"};
+    }])
+
+    .controller('ScopeThreeController',['$scope', function($scope){
+
     }]);
